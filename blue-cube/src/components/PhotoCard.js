d89803a6315f0187dd4f68a13d5dc2f3d2e5ffae
easymagic <nnamware@yahoo.com>
2021-10-23 15:18:26 +0100

    np

--- blue-cube/src/components/PhotoCard.js
+++ blue-cube/src/components/PhotoCard.js
@@ -2,13 +2,27 @@ export const PhotoCard = ({src})=>{
 
     return (<>
        
-       <div className="w-1/5  p-3">
+       <div className="w-1/5  p-3 relative">
 
            <div className="h-60 rounded" style={{
            backgroundImage:`url(${src})`,
            backgroundSize:'cover'
        }}>
 
+           </div>
+
+           <div className="relative -top-12 bg-white w-full p-2 rounded-bl rounded-br border-b border-l border-r">
+
+               <div>
+                   Name
+               </div>
+               <div>
+                   Location
+               </div>
+               <div>
+                   Actions
+               </div>
+  
            </div>
 
        {/* <img src={src} className="w-full" /> */}
